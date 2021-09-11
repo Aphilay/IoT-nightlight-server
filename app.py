16d49cfcefc772784b0a98df5bc65d8e18cbe085
Alex <aphilay@gmail.com>
2021-09-11 02:00:04 -0700

    added comments

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import paho.mqtt.client as mqtt
 id = '0eae5b2e-4fb0-42cb-a639-f824ccaae930'
 
 client_telemetry_topic = id + '/telemetry'
-server_command_topic = id + '/commands'
 client_name = id + 'nightlight_server'
+server_command_topic = id + '/commands'
 
 mqtt_client = mqtt.Client(client_name)
 mqtt_client.connect('test.mosquitto.org')
@@ -18,11 +18,14 @@ def handle_telemetry(client, userdata, message):
     payload = json.loads(message.payload.decode())
     print("Message received:", payload)
 
+    #if value is less than 100, command = true
     command = { 'led_on' : payload['light'] < 100 }
     print("Sending message:", command)
 
+    #publish to the server_command topic
     client.publish(server_command_topic, json.dumps(command))
 
+#subscribe to the client telemetry topic to receive the light value
 mqtt_client.subscribe(client_telemetry_topic)
 mqtt_client.on_message = handle_telemetry
 
